b83b5540aaa8ab87e62304e9d5ca98b228122afe
Wester Coenraads <info@lokkij.com>
2020-07-02 01:23:40 +0200

    Add the 1st order model

--- HitlerModels.py
+++ HitlerModels.py
@@ -4,10 +4,13 @@
 
 import mlsolver
 from mlsolver.kripke import World, KripkeStructure
+from mlsolver.formula import *
 from itertools import combinations
 from random import randrange, choice, sample
 import threading
 import time
+import copy
+import numpy as np
 
 fascist_dist = {
         5:2,
@@ -150,6 +153,7 @@ class First_Order(object):
     def __init__(self,n_players):
         self.n_players = n_players
         self.n_fascists = fascist_dist[self.n_players]
+        self.fascists = sample(list(range(self.n_players)), self.n_fascists)
         self.fascist_wins_needed = 5
         self.liberal_wins_needed = 5
         self.president = 0
@@ -157,7 +161,7 @@ class First_Order(object):
         self.liberal_wins = 0
         self.fascist_wins = 0
         self.game_state = 'Start'
-        
+        self.votes = { n: False for n in range(n_players) }
         combination_numbers = combinations(range(self.n_players), self.n_fascists)
         self.possible_combinations = []
         for combo in combination_numbers:
@@ -167,19 +171,20 @@ class First_Order(object):
         self.worlds = [World("{}".format(idx), { atom: True for atom in atoms}) for idx, atoms in enumerate(self.possible_combinations)]
         self.n_worlds = len(self.worlds)
         
-        self.relations = {
-            # Just the liberal relations
-            n: [(str(x), str(y)) for x, y in list(combinations((range(self.n_worlds)), 2))] for n in range(self.n_fascists, self.n_players)
-        }
-        
-        # Add the fascist relations
-        for n in range(self.n_fascists):
-            self.relations.update({ n: [] })
-        
-        self.relations.update(self.add_reflexive_edges(self.worlds, self.relations))
-        self.relations.update(self.add_symmetric_edges(self.relations))
-        
-        self.fascists = sample(list(range(self.n_players)), self.n_fascists)
+        self.models = []
+        for player in range(self.n_players):
+            worlds = copy.deepcopy(self.worlds)
+            relations = {
+                # Just the liberal relations
+                (str(x), str(y)) for x, y in list(combinations((range(self.n_worlds)), 2))
+            } if self.is_liberal(player) else set()
+
+            relations.update(self.add_reflexive_edges(worlds, relations))
+            relations.update(self.add_symmetric_edges(relations))
+            model = KripkeStructure(worlds, relations)
+            atom = Atom('{}=fascist'.format(player)) if self.is_fascist(player) else Atom('{}=liberal'.format(player))
+            model = model.solve(atom)
+            self.models.append(model)
 
         # t1 = threading.Thread(target=self.run_game)
         # t1.start()
@@ -190,23 +195,17 @@ class First_Order(object):
     def is_liberal(self,player_number):
         return not self.is_fascist(player_number)
     
-    def add_reflexive_edges(self,worlds, relations):
-        result = {}
-        for agent, agents_relations in relations.items():
-            result_agents = agents_relations.copy()
-            for world in worlds:
-                result_agents.append((world.name, world.name))
-                result[agent] = result_agents
+    def add_reflexive_edges(self, worlds, relations):
+        result = set()
+        for world in worlds:
+            result.add((world.name, world.name))
         return result
     
     def add_symmetric_edges(self,relations):
-        result = {}
-        for agent, agents_relations in relations.items():
-            result_agents = agents_relations.copy()
-            for r in agents_relations:
-                x, y = r[1], r[0]
-                result_agents.append((x, y))
-            result[agent] = result_agents
+        result = set()
+        for r in relations:
+            x, y = r[1], r[0]
+            result.add((x, y))
         return result
     
     def run_game(self):
@@ -219,18 +218,56 @@ class First_Order(object):
         while self.liberal_wins < self.liberal_wins_needed and self.fascist_wins < self.fascist_wins_needed:
             self.game_state = "Choosing Government"
             self.president = (self.president + 1) % self.n_players
-            if self.is_liberal(self.president):
-                pass
+            
+            # Choosing a chancellor
+            if self.is_fascist(self.president):
+                self.chancellor = choice(list(range(self.president)) + list(range(self.president + 1, self.n_players)))
             else:
-                # Potential issue: this crashes if there is only 1 fascist in the game
-                pass
-                
-            votes_for = randrange(0, self.n_players - self.n_fascists + 1) + (self.n_fascists if self.is_fascist(self.chancellor) else 0)
+                model = self.models[self.president]
+                # Choose the 'most liberal' candidate
+                counts = []
+                for player in range(self.n_players):
+                    counts.append(sum(map(lambda w: 1 if w.assignment.get('{}=liberal'.format(player), False) else 0, model.worlds)))
+                counts[self.president] = -1
+                self.chancellor = np.argmax(counts)
+
+            # Voting on the government
+            votes_for = 0
+            for player in range(self.n_players):
+                if player == self.president or (player == self.chancellor and self.is_fascist(player)):
+                    self.votes[player] = True
+                    votes_for += 1
+                elif self.is_liberal(player):
+                    f = Or(Atom('{}=fascist'.format(self.president)), Atom('{}=fascist'.format(self.chancellor)))
+                    one_is_fascist = copy.deepcopy(self.models[player]).solve(f) == self.models[player]
+                    vote = not one_is_fascist
+                    self.votes[player] = vote
+                    votes_for += 1 if vote else 0
+                else:
+                    vote = self.is_fascist(self.president) or self.is_fascist(self.chancellor)
+                    self.votes[player] = vote
+                    votes_for += 1 if vote else 0
+
+            # The government chooses a policy
+            # and the kripke models are updated
+            # based on the policy chosen
             if votes_for >= vote_threshold:
                 if self.is_liberal(self.president) and self.is_liberal(self.chancellor):
                     self.liberal_wins += 1
+                    updater = Box(And(Atom('{}=liberal'.format(self.president)), Atom('{}=liberal'.format(self.chancellor))))
+                    for n in range(self.n_players):
+                        self.models[n] = self.models[n].solve(updater)
                 else:
                     self.fascist_wins += 1
+                    updater = Box(Or(Atom('{}=fascist'.format(self.president)), Atom('{}=fascist'.format(self.chancellor))))
+                    for n in range(self.n_players):
+                        self.models[n] = self.models[n].solve(updater)
+                    
+                    if self.is_liberal(self.president):
+                        self.models[self.president] = self.models[self.president].solve(Atom('{}=fascist'.format(self.chancellor)))
+                    if self.is_liberal(self.chancellor):
+                        self.models[self.chancellor] = self.models[self.chancellor].solve(Atom('{}=fascist'.format(self.president)))
+                    
         
         if self.liberal_wins >= 5:
             self.winner = 0
